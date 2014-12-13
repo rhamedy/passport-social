02f659dfb096661b51fd2f7a6cded75d7202ef57
Agraj Mangal <amangal@adobe.com>
2014-12-14 01:36:38 +0530

    Second commit

--- twitter.js
+++ twitter.js
@@ -1,6 +1,6 @@
 // twitter app settings
 module.exports = {
-	'apikey' : 'BtvMEx0q3jEH682nwy7noDj0t',
-	'apisecret' : 'i5evDhbxwIbKTyohVDj6PXPz2CRKEDHjfS0xqVuEoGLhwenIos',
+	'apikey' : '<your_api_key>',
+	'apisecret' : '<your_api_secret>',
 	'callbackUrl' : 'http://127.0.0.1:3000/login/twitter/callback'
 }
